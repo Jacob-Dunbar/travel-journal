6075e80dd9f9910c0b26e25552cfd5f5dfc71979
Jacob-Dunbar <jacob.dunbar@googlemail.com>
2022-10-11 17:00:55 +0100

    chore: add card component and pass data

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import Header from "./Components/Header.jsx";
+import data from "./data";
+import Card from "./Components/Card";
+
+const cards = data.map((item) => <Card key={item.title} {...item} />);
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <section className="cards">{cards}</section>
     </div>
   );
 }
---
+++ src/Components/Card.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
+
+const Card = (props) => {
+  return (
+    <div className="card">
+      {/* <img className="card__image" src={props.imageUrl} alt="" /> */}
+      <div className="card__content">
+        <div className="card__content__location">
+          <FontAwesomeIcon
+            className="content__location__icon"
+            icon={faLocationDot}
+          />
+          <h2 className="content__location__text">{props.location}</h2>
+          <a href={props.googleMapsUrl} className="content__location_link">
+            View on Google Maps
+          </a>
+        </div>
+        <h1 className="card__content__title">{props.title}</h1>
+        <h3 className="card__content__dates">
+          {props.startDate} - {props.endDate}
+        </h3>
+        <p className="card__content__description">{props.description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
