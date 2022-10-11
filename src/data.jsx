a65c568d57155696a90398b3bdb61791b89b9793
Jacob-Dunbar <jacob.dunbar@googlemail.com>
2022-10-11 15:20:46 +0100

    chore: add data.js and images

---
+++ src/data.jsx
@@ -0,0 +1,32 @@
+export default [
+  {
+    title: "Hoi An",
+    location: "Vietnam",
+    googleMapsUrl: "https://goo.gl/maps/3ZTQdjeA2SuzQURp6",
+    startDate: "16 Aug, 2022",
+    endDate: "20 Aug, 2022",
+    description:
+      "Hoi An is a beautiful Vietnamese town, famous for its display of lanterns that take to Thu Bon River every night. The food here was the best I have ever eaten.",
+    imageUrl: "src/assets/hoian.jpg",
+  },
+  {
+    title: "Berlin",
+    location: "Germany",
+    googleMapsUrl: "https://goo.gl/maps/gXC8z1HMmZdnmwny6",
+    startDate: "18 Mar, 2022",
+    endDate: "21 Mar, 2022",
+    description:
+      "Berlin is an interesting city with a fascinating and dark history. The city today however is vibrant and beautiful, with eclectic culture, incredible food and wild nightlife.",
+    imageUrl: "src/assets/berlin.jpg",
+  },
+  {
+    title: "Siquijor",
+    location: "The Philippines",
+    googleMapsUrl: "https://goo.gl/maps/oXgZjSYEXotphMUZ6",
+    startDate: "19 Nov, 2019",
+    endDate: "22 Nov, 2019",
+    description:
+      "Siquior is a tiny and mysterious island province, famous (or infamous) for witchcraft and the unknown. However, my experience was more of fresh seafood and cocktails, than of spells and potions. Certainly still magical.",
+    imageUrl: "src/assets/siquior.jpg",
+  },
+];
